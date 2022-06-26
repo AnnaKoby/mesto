70035cbade89a93c2ac3b8c7b93130881d528526
Anna Kobylina <anuta.kobylina@yandex.ru>
2022-06-26 17:58:30 +0300

    fixed preview transparency

---
+++ scripts/cards.js
@@ -0,0 +1,27 @@
+//массив карточек появляющихся по умолчанию
+const initialCards = [
+    {
+        name: 'Удмуртия',
+        link: 'https://pbs.twimg.com/media/FM6VbegXwAURqlI.jpg'
+    },
+    {
+        name: 'Ижевск',
+        link: 'https://avatars.mds.yandex.net/get-zen_doc/1908497/pub_605090ade781846a40ad1577_605090f90a7d51654ace9a25/scale_1200'
+    },
+    {
+        name: 'Сарапул',
+        link: 'https://pro-sarapul.ru/images/photo/usadyba-bashenina-sarapul-208.jpg'
+    },
+    {
+        name: 'Глазов',
+        link: 'https://s12.stc.yc.kpcdn.net/share/i/12/10752003/wr-960.webp'
+    },
+    {
+        name: 'Воткинск',
+        link: 'https://avatars.mds.yandex.net/get-zen_doc/2352663/pub_5fc92ec76bcad04cbc676afa_5fc92f9852642f33b9b60acf/scale_1200'
+    },
+    {
+        name: 'Можга',
+        link: 'https://express-china.ru/upload/iblock/910/mozhga.jpg'
+    }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,98 +1,66 @@
 const cardElement = document.querySelector('template');
 const cardElementsNode = document.querySelector('.elements');
 
-const button = document.querySelector('.profile__pen-button'); // переменная для обращения кнопки "карандаш"
-const popup = document.querySelector('#profile-add'); // переменная для обращения к окну изменения имени пользоваеля
-const popupCloseButton = document.querySelector('.popup__close-button'); // переменная для закрытия окна изменения имени пользователя
-const formElement = document.querySelector('.popup__form'); //переменная для доступа к элементам ввода в окне изменения имени пользователя
-const nameInput = document.querySelector('.popup__input_type_name'); //переменная для доступа к первой строке ввода окна изменения имени пользователя
-const jobInput = document.querySelector('.popup__input_type_description'); //переменная для доступа ко второй строке ввода 
+const setProfileButton = document.querySelector('.profile__pen-button'); // переменная для обращения кнопки "карандаш"
+const setProfilePopup = document.querySelector('#profile-add'); // переменная для обращения к окну изменения имени пользоваеля
+const setProfilePopupCloseButton = document.querySelector('.popup__close-button'); // переменная для закрытия окна изменения имени пользователя
+const setProfileFormElement = document.querySelector('.popup__form'); //переменная для доступа к элементам ввода в окне изменения имени пользователя
+const setProfileNameInput = document.querySelector('.popup__input_type_name'); //переменная для доступа к первой строке ввода окна изменения имени пользователя
+const setProfileJobInput = document.querySelector('.popup__input_type_description'); //переменная для доступа ко второй строке ввода 
 const profileName = document.querySelector('.profile__name'); //переменная для доступа к месту отражения первой строки на странице
 const profileDescription = document.querySelector('.profile__description'); //переменная для доступа к месту отражения второй строки на странице
 
-const newCardpopup = document.querySelector('#card-add');//переменная для обращения изменения новой карточки
+const newCardPopup = document.querySelector('#card-add');//переменная для обращения изменения новой карточки
 const newCardButton = document.querySelector('.profile__add-button');// кнопка для добавления новой каточки мест
-const newCardpopupCloseButton = newCardpopup.querySelector('.popup__close-button');//переменная для закрытия окна изменения новой карточки
-const newCardformElement = newCardpopup.querySelector('.popup__form');//переменная для доступа к элементам ввода в окне изменения новой карточки
-const newCardName = newCardpopup.querySelector('.popup__input_name');//переменная для доступа к первой строке ввода новой карточки
-const newCardLink = newCardpopup.querySelector('.popup__input_link');//переменная для доступа ко второй строке ввода новой карточки
+const newCardPopupCloseButton = newCardPopup.querySelector('.popup__close-button');//переменная для закрытия окна изменения новой карточки
+const newCardFormElement = newCardPopup.querySelector('.popup__form');//переменная для доступа к элементам ввода в окне изменения новой карточки
+const newCardName = newCardPopup.querySelector('.popup__input_name');//переменная для доступа к первой строке ввода новой карточки
+const newCardLink = newCardPopup.querySelector('.popup__input_link');//переменная для доступа ко второй строке ввода новой карточки
 
 const largeImagePopup = document.querySelector('#large-image');//переменная для открытия просмотра картинки
 const largeImagePopupCloseButton = largeImagePopup.querySelector('.popup__close-button');//переменная для закрытия просмотра картинки
 
-//массив карточек появляющихся по умолчанию
-const initialCards = [
-  {
-    name: 'Ижевск',
-    link: 'https://avatars.mds.yandex.net/get-zen_doc/1908497/pub_605090ade781846a40ad1577_605090f90a7d51654ace9a25/scale_1200'
-  },
-  {
-    name: 'Сарапул',
-    link: 'https://pro-sarapul.ru/images/photo/usadyba-bashenina-sarapul-208.jpg'
-  },
-  {
-    name: 'Глазов',
-    link: 'https://s12.stc.yc.kpcdn.net/share/i/12/10752003/wr-960.webp'
-  },
-  {
-    name: 'Воткинск',
-    link: 'https://avatars.mds.yandex.net/get-zen_doc/2352663/pub_5fc92ec76bcad04cbc676afa_5fc92f9852642f33b9b60acf/scale_1200'
-  },
-  {
-    name: 'Можга',
-    link: 'https://express-china.ru/upload/iblock/910/mozhga.jpg'
-  },
-  {
-    name: 'Удмуртия',
-    link: 'https://pbs.twimg.com/media/FM6VbegXwAURqlI.jpg'
-  }
-];
-
 // функция делающая попап невидимым
 function closePopup() {
-  popup.classList.remove('popup_opened');
-  popupCloseButton.removeEventListener('click', closePopup);
-  popup.removeEventListener('click', closePopupOnClick);
+  setProfilePopup.classList.remove('popup_opened');
+  setProfilePopupCloseButton.removeEventListener('click', closePopup);
+  setProfilePopup.removeEventListener('click', closePopupOnClick);
   document.body.removeEventListener('keyup', closePopupOnEscape);
-  formElement.removeEventListener('submit', formSubmitHandler);
+  setProfileFormElement.removeEventListener('submit', formSubmitHandler);
 }
-
 // Обработчик «отправки» формы
 function formSubmitHandler(evt) {
   evt.preventDefault();
   //меняю текст
-  profileName.textContent = nameInput.value;
-  profileDescription.textContent = jobInput.value;
+  profileName.textContent = setProfileNameInput.value;
+  profileDescription.textContent = setProfileJobInput.value;
   //закрываем попап
   closePopup();
 }
-
 // функция закрывающая попап при нажатии esc
 function closePopupOnEscape(e) {
   if (e.key === 'Escape') {
     closePopup();
   }
 }
-
 // функция закрывающая попап при нажатии на страницу вне попапа
 function closePopupOnClick(e) {
   if (e.target === e.currentTarget) {
     closePopup();
   }
 }
-
 //действия необходимые для открытия попапа изменения имени пользователя
 function openPopup() {
-  popup.classList.add('popup_opened');
-  popupCloseButton.addEventListener('click', closePopup);
-  popup.addEventListener('click', closePopupOnClick);
+  setProfilePopup.classList.add('popup_opened');
+  setProfilePopupCloseButton.addEventListener('click', closePopup);
+  setProfilePopup.addEventListener('click', closePopupOnClick);
   document.body.addEventListener('keyup', closePopupOnEscape);
   // Прикрепляем обработчик к форме:
   // он будет следить за событием “submit” - «отправка»
-  formElement.addEventListener('submit', formSubmitHandler);
+  setProfileFormElement.addEventListener('submit', formSubmitHandler);
   //установим значение полей ввода
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileDescription.textContent;
+  setProfileNameInput.value = profileName.textContent;
+  setProfileJobInput.value = profileDescription.textContent;
 }
 //закрытие попапа, когда добавляю новую карточку
 function closeNewCardPopupOnClick(e) {
@@ -116,29 +84,27 @@ function formNewCardSubmitHandler(evt) {
 }
 //действия закрытия попапа добавляющего новую карточку
 function closeNewCardPopup() {
-  newCardpopup.classList.remove('popup_opened');
-  newCardpopupCloseButton.removeEventListener('click', closeNewCardPopup);
-  newCardpopup.removeEventListener('click', closeNewCardPopupOnClick);
+  newCardPopup.classList.remove('popup_opened');
+  newCardPopupCloseButton.removeEventListener('click', closeNewCardPopup);
+  newCardPopup.removeEventListener('click', closeNewCardPopupOnClick);
   document.body.removeEventListener('keyup', closeNewCardPopupOnEscape);
-  newCardformElement.removeEventListener('submit', formNewCardSubmitHandler);
+  newCardFormElement.removeEventListener('submit', formNewCardSubmitHandler);
 }
-
 //действия необходимые для открытия попапа добавления новой карточки с фотографией
 function openNewCardPopup() {
-  newCardpopup.classList.add('popup_opened');
-  newCardpopupCloseButton.addEventListener('click', closeNewCardPopup);
-  newCardpopup.addEventListener('click', closeNewCardPopupOnClick);
+  newCardPopup.classList.add('popup_opened');
+  newCardPopupCloseButton.addEventListener('click', closeNewCardPopup);
+  newCardPopup.addEventListener('click', closeNewCardPopupOnClick);
   document.body.addEventListener('keyup', closeNewCardPopupOnEscape);
   // Прикрепляем обработчик к форме:
   // он будет следить за событием “submit” - «отправка»
-  newCardformElement.addEventListener('submit', formNewCardSubmitHandler);
+  newCardFormElement.addEventListener('submit', formNewCardSubmitHandler);
   //установим значение полей ввода
   newCardName.value = "";
   newCardLink.value = "";
 }
-
 // открытие попап при нажатии на "карандаш"
-button.addEventListener('click', function () {
+setProfileButton.addEventListener('click', function () {
   openPopup();
 });
 //обработка события по нажатию кнопки добавления новой карточки
@@ -188,22 +154,20 @@ function closeLargeImagePopupOnClick(e) {
 //действия для открытия картинки
 function PreviewImageLarge(ElementToPreview) {
   largeImagePopup.classList.add('popup_opened');//делаем попап окна просмотра картинки видимым
+  largeImagePopup.classList.add('popup__large-image');
   largeImagePopupCloseButton.addEventListener('click', closeLargeImagePopup);
   largeImagePopup.addEventListener('click', closeLargeImagePopupOnClick);
   document.body.addEventListener('keyup', closeLargeImagePopupOnEscape);
   imagePreview = largeImagePopup.querySelector(".popup__large-image-preview");
   imageCaption = largeImagePopup.querySelector(".popup__image-title");
-  debugger;
   imagePreview.src = ElementToPreview.src;
   imagePreview.alt = ElementToPreview.alt;
   imageCaption.innerText = ElementToPreview.alt;
-
 }
 //открытие окна просмотра картинки
 function largeImagePress(evt) {
   PreviewImageLarge(evt.target);
 }
-
 function addNewCard(name,link) {
   let elementToAdd = cardElement.content.cloneNode(true);
   //elementToAdd.querySelector(".elements__card").setAttribute('id', `card${currentCard.id}`);
@@ -218,13 +182,11 @@ function addNewCard(name,link) {
   binButton.addEventListener('click', binButtonPress);
   cardElementsNode.appendChild(elementToAdd);
 }
-
 //добавление карточек 
 function showInitialContent() {
   initialCards.forEach((currentCard) => {
     addNewCard(currentCard.name,currentCard.link);
   })
 }
-
 //покажем карточки, которые пока не видимы
 showInitialContent();
